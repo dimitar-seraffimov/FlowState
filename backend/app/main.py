8bd0690c25021b810ce126f2ba6f2365231e55e6
dimitar-seraffimov <dimitar.seraffimov@gmail.com>
2025-02-15 13:52:39 +0000

    fronted should be working

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from routers import session
 from fastapi.responses import HTMLResponse
-from fastapi.requests import Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import os
@@ -9,18 +8,23 @@ app = FastAPI()
 
 # creates the static path
 static_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../", "static")
+templates = Jinja2Templates(directory="templates")
+
 
 # mounts the static files directory to the app
 app.mount("/static", StaticFiles(directory=static_path), name="static")
 
 app.include_router(session.router)
 
-templates = Jinja2Templates(directory="templates")
 
 # serve index.html
 @app.get("/", response_class=HTMLResponse)
-def read_root(request: Request):
+async def read_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/session-setup", response_class=HTMLResponse)
+async def read_session_setup(request: Request):
+    return templates.TemplateResponse("session-setup.html", {"request": request})
 
 
 
